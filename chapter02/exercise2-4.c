7e44f5c397ddc0484cfb972fef2bc2090aab425e
Randip Bhachu <randipsbh@gmail.com>
2020-03-25 17:33:50 +0000

    exercise2-4.c

--- chapter02/exercise2-4.c
+++ chapter02/exercise2-4.c
@@ -5,20 +5,14 @@ void squeeze(char s1[], char s2[])
 {
   int i, j, k, ok;
   for (i = k = 0; s1[i] != '\0'; i++) {
-    printf("i = %d:\n", i);
     ok = 1;
     for (j = 0; s2[j] != '\0'; j++) {
-      printf("  j = %d\n", j);
       if (s1[i] == s2[j]) {
         ok = 0;
       }
     }
     if (ok) {
-      printf("ok = %d, k = %d, i = %d\n", ok, k, i);
-      printf("  %c\n", s1[k++]);
-      printf("  %c\n", s1[i]); 
       s1[k - 1] = s1[i];
-      printf("end ok\n");
     }
   }
   s1[k] = '\0';
@@ -34,8 +28,6 @@ main()
   char s1[] = "hello world";
   char s2[] = "oe";
   char expected[] = "hll wrld";
-  printf("call squeeze\n");
   squeeze(s1, s2);
-  printf("call check\n");
   check("hello world", s2, s1, expected);
 }
